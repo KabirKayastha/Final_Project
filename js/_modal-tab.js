3f72c819cacf03ff14c0b80917bf7b1aed23ee78
KabirKayastha <kayas.kabie@gmail.com>
2019-01-07 19:27:50 +0545

    modal interface completed, elements left to be added

---
+++ js/_modal-tab.js
@@ -0,0 +1,39 @@
+var tabButtons=document.querySelectorAll(".tabContainer .buttonContainer button");
+var tabPanels=document.querySelectorAll(".tabContainer .tabPanel");
+
+
+function showPanel(panelIndex,opacityAmount) {
+	tabButtons.forEach(function(node){
+		node.style.opacity="";
+		node.style.color="";
+	});
+	tabButtons[panelIndex].style.opacity=opacityAmount;
+	tabButtons[panelIndex].style.color="white";
+	
+	tabPanels.forEach(function(node){
+		node.style.display="NONE"; 
+	});
+	tabPanels[panelIndex].style.display="block";
+	//tabPanels[panelIndex].style.backgroundColor=colorCode;
+}
+
+function showPassword() {
+	var x=document.getElementById("password");
+	if (x.type === "password") {
+		x.type = "text";
+	}
+	
+}
+
+function hide() {
+	// body...
+	var x=document.getElementById("password");
+
+	if(x.type === "text"){
+		x.type = "password";
+	}
+}
+
+showPanel(0,1);
+
+
